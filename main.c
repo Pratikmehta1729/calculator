c172906aef5d3611aa7980aa52a8d55803198824
Genius <36341114+deepamgupta@users.noreply.github.com>
2018-10-08 12:25:37 +0530

    Update main.c

--- main.c
+++ main.c
@@ -9,8 +9,17 @@
 
 void main(){
   int x,y;
-  printf("Enter 2 Numbers : ");
-  scanf("%d%d",&x,&y);
-  int c=add();
-  printf("%d",c);
+  printf("Enter 2 Numbers : \n");
+  printf("X : \n");
+  scanf("%d",&x);
+  printf("Y : \n");
+  scanf("%d",&y);
+  printf("Sum : %d\n",add(x, y));
+  printf("Sub : %d\n",sub(x, y));
+  printf("Power : %d\n",powerx, y));
+  printf("Quotient : %d\n",division(x, y));
+  printf("Multiply : %d\n",multiply(x, y));
+  printf("Remainder : %d\n",remainder(x, y));
+  printf("Power : %d\n",pow(x, y));
+  printf("Sqrt : %d\n",sqrt(x, y));
 }
